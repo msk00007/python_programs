e2afc600a3361cfb513c328efbe192d20d72e366
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-08-27 23:30:33 +0530

    Add files via upload

---
+++ test24.py
@@ -0,0 +1,20 @@
+
+def isprime(num):
+    flag = True
+    if num == 2:
+        return flag 
+    for i in range(2,int(num)):
+        if int(num) % i == 0:
+            flag = False
+            break
+    return flag
+
+
+N = int(input("enter the number \n"))
+if N>2:
+    for i in range(2,N):
+        result = isprime(i)
+        if result :
+            print(i, end = " ")
+else:
+    print("not a prime number")
