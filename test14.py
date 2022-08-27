9909c97025de05a63d82c2239952a889bc7e9e95
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-08-27 18:59:08 +0530

    Update test14.py

--- test14.py
+++ test14.py
@@ -1,13 +1,7 @@
-from email import header
-
-
 class node:
     def __init__(self,data):
         self.data = data
         self.Next = None
-
-
-
 class linkedlist():
     def __init__(self):
         self.head = None
@@ -26,12 +20,40 @@ class linkedlist():
                     itr = itr.Next
                 new_node = node(element)
                 itr.Next = new_node
-
+    def delete(self,delelem):
+        itr = self.head
+        if itr is None :
+            print("list is empty...")
+            return
+        elif itr.data == delelem :
+            self.head = itr.Next
+            itr = None
+            return
+        prev = itr
+        while itr is not None and itr.data is not delelem :
+            prev = itr
+            itr = itr.Next
+        if itr is None:
+            print("element is not found ...")
+            return
+        elif itr.data == delelem :
+            prev.Next = itr.Next
+    def printlist(self):
+        if self.head is None:
+            print("list is empty cant print list...")
+            return
+        itr = self.head
+        while itr is not None:
+            print(itr.data, end = " ")
+            itr = itr.Next
+        print()
 l = linkedlist()
-l.insert(5)
 l.insert(55)
+l.insert(5)
+l.insert(56)
 l.insert(555)
-itr = l.head
-while itr is not None:
-    print(itr.data, end = " ")
-    itr = itr.Next
+l.printlist()
+l.delete(555)
+l.printlist()
+l.insert(555)
+l.printlist()
