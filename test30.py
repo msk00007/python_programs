face9dd8bc63d14ecd0f4719d317f9dee1425745
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-08-28 19:38:16 +0530

    Add files via upload

---
+++ test30.py
@@ -0,0 +1,40 @@
+class deque:
+    def __init__(self):
+        self.l=[]
+    def AddAtFront(self,element):
+        self.l.insert(0,element)
+    def AddAtEnd(self,element):
+        self.l.append(element)
+    def isEmpty(self):
+        if len(self.l)==0:
+            return True
+        return False
+    def delAtEnd(self):
+        if self.isEmpty():
+            print("list is empty")
+            return
+        else:
+            delitem = self.l.pop()
+            return delitem  
+    def delAtFirst(self):
+        if self.isEmpty():
+            print("list is empty")
+            return
+        else:
+            delitem = self.l.pop(0)
+            return delitem
+    def size(self):
+        return len(self.size)
+    def display(self):
+        for each in self.l:
+            print(each,end = " ")
+        print()
+
+Deque = deque()
+Deque.AddAtEnd(6)
+Deque.AddAtEnd(67)
+Deque.AddAtFront(667)
+Deque.display()
+Deque.delAtEnd()
+Deque.display()
+        
