6811257f7980c6c88310222ae6d862dc4e5bb90b
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-08-28 14:46:19 +0530

    Update test6.py

--- test6.py
+++ test6.py
@@ -1,5 +1,5 @@
 class complex:
-    def __init__(self, x=0, y=0):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
@@ -7,12 +7,20 @@ class complex:
          print("{0}+i{1}".format(self.x, self.y))
 
     def __add__(self, other):
-        x = self.x + other.x
-        y = self.y + other.y
-        return complex(x, y)
+        return complex(self.x + other.x, self.y + other.y)
+    def __sub__(self,other):
+        return complex(self.x-other.x,self.y-other.y)
+    def __mul__(self,other):
+        x = self.x*other.x - (self.y*other.y)
+        y = self.x*other.y + self.y*other.x
+        return(complex(x,y))
 
 
 p1 = complex(5,4)
 p2 = complex(2, 3)
 p3=p1+p2
+p4 = p1-p2
+p5 = p1*p2
 p3.display()
+p4.display()
+p5.display()
