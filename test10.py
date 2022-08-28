4de5ac3ad11dc92615fee15bed2a5418e7902caf
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-08-28 22:54:14 +0530

    Update test10.py

--- test10.py
+++ test10.py
@@ -1,16 +1,28 @@
-def Bsort(l):
-    print("bubblesort is invoked.....")
-    n=len(l)
-    for i in range(n-1):
-         for j in range(n-1-i):
-             if(l[j]>l[j+1]):
-                l[j],l[j+1]=l[j+1],l[j]
-
+def Bsearch(l,lb,ub,key):
+    if lb<=ub:
+        mid = (lb+ub)//2
+        if l[mid]==key:
+            return mid
+        elif key<l[mid]:
+            for i in range(lb,mid):
+                if l[i]==key:
+                    return i
+        elif key>l[mid]:
+            for i in range(mid+1,ub+1):
+                if l[i]==key:
+                    return i
+    return -1
 list=[]
 n=int(input("Enter the range of the list \n"))
+if n<=0:
+    print("enter valid range..")
+    exit()
 print("enter the elements")
 for i in range(n):
     list.append(int(input()))
-print(list)
-Bsort(list)
-print("the sorted list is : ",list)
+key = int(input("Enter the key to search..\n"))
+result = Bsearch(list,0,n-1,key)
+if result != -1:
+    print("key value is find at index : ",result)
+else:
+    print("key value is not found...")
