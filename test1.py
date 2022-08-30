51d95ec8d957861e91c7dc95d4d0f42f57fa6e1b
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-08-30 03:47:43 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 list = []
-for i in range(3):
+N = int(input(" enter the range of list..\n"))
+for i in range(N):
     l=[]
     l.append(input("enter the name  \n"))
     l.append(int(input("enter the age \n")))
