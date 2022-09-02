5f2354825544efcedccbf40ff66b7d9a9a683181
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-09-02 17:00:38 +0530

    Update test29.py

--- test29.py
+++ test29.py
@@ -1,14 +1,8 @@
 import sys
-try:
-    try:
-        inputfile = open(sys.argv[1],"r")
-        print("the path of the file is ",sys.argv[1])
-        print(inputfile.read())
-    except:
-        print("file is not found kindly give correct path")
-    finally:
-        inputfile.close()
-except:
-    print("some error accured while  opening the file..")
-finally:
-    print("try block is completed")
+
+inputfile = open(sys.argv[1],"r")
+# print("the path of the file is ",sys.argv[1])
+N = int(sys.argv[2])
+inputfile.read(N)
+print(inputfile.read())
+inputfile.close()
