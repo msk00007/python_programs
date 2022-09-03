765bdd1dc35ad065914e52841c74c3abdfd92303
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-09-03 22:38:54 +0530

    Update test17.py

--- test17.py
+++ test17.py
@@ -1,9 +1,9 @@
 class node:
     def __init__(self,data):
         self.data = data
-        self.front = None
-        self.rear = None
-class linkedlist():
+        self.next = None
+        self.prev = None
+class doublylinkedlist():
     def __init__(self):
         self.head = None
 
@@ -14,53 +14,49 @@ class linkedlist():
             return
         else:
             new_node = node(element)
-            new_node.front = self.head
-            self.head.rear = new_node
+            new_node.next = self.head
+            self.head.prev = new_node
             self.head = new_node
 
     def enqueAtEnd(self,element):
+        new_node = node(element)
         if self.head ==None:
-            new_node = node(element)
             self.head = new_node
             return
         else:
-            if self.head.front == None:
-                new_node = node(element)
-                new_node.rear = self.head
-                self.head.front = new_node
-                
+            if self.head.next == None:
+                new_node.prev = self.head
+                self.head.next = new_node     
             else:
                 itr = self.head
-                while itr.front != None:
-                    itr = itr.front
-                new_node = node(element)
-                new_node.rear = itr
-                itr.front = new_node
+                while itr.next != None:
+                    itr = itr.next
+                new_node.prev = itr
+                itr.next = new_node
     def dequeAtEnd(self):
         itr = self.head
         if itr is None :
             print("list is empty...")
             return
-        elif itr.front == None :
-            self.head = itr.front
+        elif itr.next == None :
+            self.head = itr.next
             itr = None
             return
-        while itr.front is not None :
-            itr = itr.front
-        itr.rear.front = itr.front
+        while itr.next is not None :
+            itr = itr.next
+        itr.prev.next = itr.next
         itr = None
     def dequeAtFirst(self):
         itr = self.head
         if itr is None:
             print("list is empty .....")
-            return
-        elif itr.front == None:
-            self.head = itr.front
+        elif itr.next == None:
+            self.head = itr.next
             itr = None
-            return
         else:
-            itr.front.rear = itr.rear
-            self.head = itr.front
+            itr.next.prev = itr.prev
+            self.head = itr.next
+            itr= None
     def display(self):
         if self.head is None:
             print("list is empty cant print list...")
@@ -68,14 +64,13 @@ class linkedlist():
         itr = self.head
         while itr is not None:
             print(itr.data, end = " ")
-            itr = itr.front
+            itr = itr.next
         print()
     def isEmpty(self):
         if self.head is None:
             return True
-        else:
-            return False
-Queue = linkedlist()
+        return False
+Queue = doublylinkedlist()
 print(Queue.isEmpty())
 Queue.enqueAtEnd(5)
 Queue.enqueAtEnd(58)
@@ -86,5 +81,7 @@ Queue.enqueAtfirst(1)
 Queue.enqueAtfirst(10)
 Queue.display()
 Queue.dequeAtFirst()
+Queue.display()
+Queue.enqueAtfirst(15)
 Queue.display()
 print(Queue.isEmpty())
