fcb4302f74ae633a855cab092ec2133886b5004d
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-09-03 22:18:09 +0530

    Update test15.py

--- test15.py
+++ test15.py
@@ -1,8 +1,8 @@
 class node:
     def __init__(self,data):
         self.data = data
-        self.front = None
-        self.rear = None
+        self.next = None
+        self.prev = None
 class linkedlist():
     def __init__(self):
         self.head = None
@@ -12,37 +12,34 @@ class linkedlist():
             self.head = new_node
             return
         else:
-            if self.head.front == None:
+            if self.head.next == None:
                 new_node = node(element)
-                new_node.rear = self.head
-                self.head.front = new_node
+                new_node.prev = self.head
+                self.head.next = new_node
                 
             else:
                 itr = self.head
-                while itr.front != None:
-                    itr = itr.front
+                while itr.next != None:
+                    itr = itr.next
                 new_node = node(element)
-                new_node.rear = itr
-                itr.front = new_node
+                new_node.prev = itr
+                itr.next = new_node
     def delete(self,delelem):
         itr = self.head
         if itr is None :
             print("list is empty...")
             return
         elif itr.data == delelem :
-            self.head = itr.front
+            self.head = itr.next
+            self.head.prev = None
             itr = None
             return
         while itr is not None and itr.data is not delelem :
-            itr = itr.front
+            itr = itr.next
         if itr is None:
             print("element is not found ...")
-            return
-        elif itr.data == delelem and itr.front == None:
-            itr.rear.front = None
-            itr = None
         else:
-            itr.rear.front = itr.front
+            itr.prev.next = itr.next
             itr = None
     def printlist(self):
         if self.head is None:
@@ -51,7 +48,7 @@ class linkedlist():
         itr = self.head
         while itr is not None:
             print(itr.data, end = " ")
-            itr = itr.front
+            itr = itr.next
         print()
 l = linkedlist()
 l.insert(55)
@@ -59,7 +56,7 @@ l.insert(5)
 l.insert(56)
 l.insert(555)
 l.printlist()
-l.delete(5)
+l.delete(56)
 l.printlist()
 l.insert(555)
 l.printlist()
