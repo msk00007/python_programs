2af7f0a7d1c1220408f8d7011e4b008a4fc05c63
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-09-03 23:15:01 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -18,10 +18,8 @@ linecount = 0;
 while line!= emptystr:
     linecount += 1
     for ch in line:
-        if ch == space:
-            wordcount+=1
-        if ch == '\n':
+        if ch == space or  ch == '\n':
             wordcount+=1
     line = input_file.readline()
-print("wordcount is : ",wordcount+1)
+print("wordcount is : ",wordcount)
 print("line count is ..",linecount)
