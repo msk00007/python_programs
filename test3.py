672c5c2c9aa1fdf8837d419a5c2a03feacb9838f
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-09-03 23:23:25 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -2,14 +2,7 @@ filename = input("enter the file name..")
 input_file = open(filename,"r")
 line = input_file.readline()
 emptystr = ""
-s = ""
 charcount = 0
-while line!= emptystr:
-    for ch in line.strip('\n'):
-        charcount+=1
-    line = input_file.readline()
-print("charcount is : ",charcount)
-input_file.close()
 input_file = open(filename,"r")
 line = input_file.readline()
 wordcount = 0
@@ -17,9 +10,12 @@ space = " "
 linecount = 0;
 while line!= emptystr:
     linecount += 1
+    for ch in line.strip('\n'):
+        charcount+=1
     for ch in line:
         if ch == space or  ch == '\n':
             wordcount+=1
     line = input_file.readline()
 print("wordcount is : ",wordcount)
 print("line count is ..",linecount)
+print("charcount is : ",charcount)
