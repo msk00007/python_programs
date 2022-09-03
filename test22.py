eb160892cf4762ad9828534a8976e2d0e243be51
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-09-04 01:48:00 +0530

    Update test22.py

--- test22.py
+++ test22.py
@@ -1,16 +1,17 @@
 import sys
 def copyfile(file1,file2):
-    inputfile=open(file1,"r")
-    outputfile = open(file2,"w")
-    line = inputfile.read()
-    outputfile.write(line)
-    inputfile.close()
-    outputfile.close()
-    outputfile = open(file2,"r")
-    print(outputfile.read())
-    outputfile.close()
-
-f1 = "test1.txt"
-f2 = "copyfile.txt"
-copyfile(sys.argv[1],sys.argv[2])
+    try:
+        inputfile=open(file1,"r")
+        outputfile = open(file2,"w")
+        line = inputfile.read()
+        outputfile.write(line)
+        inputfile.close()
+        outputfile.close()
+        print("data copied successfully")
+    except:
+        print("error accured while opening the source file")
+try:
+    copyfile(sys.argv[1],sys.argv[2])
+except:
+    print("Error accured while giving input pls run again and give correct input and parameters..")
 
