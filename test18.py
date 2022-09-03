dddecb2e00c86dd4e2ef1e97ee70ae6d72c8ff63
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-09-04 00:55:05 +0530

    Add files via upload

---
+++ test18.py
@@ -0,0 +1,66 @@
+class node:
+    def __init__(self,element):
+        self.data = element
+        self.next = None
+class stacklinkedlist:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def push(self,elem):
+        new_node = node(elem)
+        itr = self.head
+        if(itr == None):
+            self.head = new_node
+            self.tail = self.head
+        else:
+            self.tail.next = new_node
+            self.tail = self.tail.next
+    def pop(self):
+        if self.head == None:
+            print("list is empty cannot pop")
+        else:
+            if self.head.next == None :
+                self.head = self.head.next
+                popelem = self.tail.data
+                self.tail = self.head
+                return popelem 
+            else:
+                itr = self.head
+                while itr.next != self.tail:
+                    itr = itr.next
+                popelem = self.tail.data
+                itr.next = None
+                self.tail = itr
+                return popelem 
+    def isEmpty(self):
+        if self.head is None:
+            return True
+        return False
+    def peek(self):
+        if self.tail != None:
+            return self.tail.data
+    def stackprint(start):
+        if start == None:
+            return
+        stacklinkedlist.stackprint(start.next)
+        print(start.data,end = " ")
+
+    def display(self):
+        if self.head == None:
+            print("stack is empty cannot print")
+        else:
+            stacklinkedlist.stackprint(self.head)
+            print()
+
+Stack = stacklinkedlist()
+print(Stack.isEmpty())
+Stack.push(4)
+Stack.push(45)
+Stack.push(456)
+Stack.display()
+Stack.pop()
+Stack.push(4567)
+Stack.display()
+
+
+                
