9c5b092b428f2388daf2f78b8dea8ad5e2944f9f
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-09-06 22:24:19 +0530

    Update test29.py

--- test29.py
+++ test29.py
@@ -3,9 +3,9 @@ try:
     try:
         inputfile = open(sys.argv[1],"r")
     except:
-        print("Error accured while opening the file..")
+        print("Error occurred ed while opening the file..")
         exit()
     inputfile.seek(int(sys.argv[2]))
     print(inputfile.read())
 except:
-    print("Errors accured for location...")
+    print("Errors occurred for location...")
