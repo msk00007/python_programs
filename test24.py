099a1ab95ba02b1ee56797be6a8327cf0c4866dc
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-09-06 21:50:49 +0530

    Update test24.py

--- test24.py
+++ test24.py
@@ -1,10 +1,10 @@
-from math import sqrt
+
 def isprime(num):
     flag = True
     if num == 2:
         return flag 
-    newrange = int(sqrt(num))
-    for i in range(2,newrange+1):
+    
+    for i in range(2,(num//2)+1):
         if int(num) % i == 0:
             flag = False
             break
