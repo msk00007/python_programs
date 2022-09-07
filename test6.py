9a93ede1c2ad57ef05a21e767b38a6e8f1a6416c
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-09-07 11:15:16 +0530

    Update test6.py

--- test6.py
+++ test6.py
@@ -4,20 +4,23 @@ class complex:
         self.y = y
 
     def display(self):
-         print("{0}+i{1}".format(self.x, self.y))
+         print("{0}+i({1})".format(self.x, self.y))
 
     def __add__(self, other):
         return complex(self.x + other.x, self.y + other.y)
     def __sub__(self,other):
-        return complex(self.x-other.x,self.y-other.y)
+        return complex(round(self.x-other.x,3),round(self.y-other.y,3))
     def __mul__(self,other):
         x = self.x*other.x - (self.y*other.y)
         y = self.x*other.y + self.y*other.x
-        return(complex(x,y))
+        return(complex(round(x,3),round(y,3)))
 
-
-p1 = complex(5,4)
-p2 = complex(2, 3)
+a = float(input("enter first real num \n"))
+b = float(input("enter first imagiary num \n"))
+c = float(input("enter second real num \n"))
+d = float(input("enter secong imaginary num \n"))
+p1 = complex(a,b)
+p2 = complex(c, d)
 p3=p1+p2
 p4 = p1-p2
 p5 = p1*p2
