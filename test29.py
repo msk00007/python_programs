370dabe89344315eff45f5f3a519fc11592c7880
msk00007 <109849322+msk00007@users.noreply.github.com>
2022-09-07 10:40:56 +0530

    Update test29.py

--- test29.py
+++ test29.py
@@ -1,11 +1,17 @@
 import sys
 try:
-    try:
-        inputfile = open(sys.argv[1],"r")
-    except:
-        print("Error occurred while opening the file..")
-        exit()
+    file = sys.argv[1]
+    location = sys.argv[2]
+except:
+    print("Error accured give correct arguments..")
+    exit()
+try:
+    inputfile = open(sys.argv[1],"r")
+except:
+    print("Error occured while opening the file ")
+    exit()
+try:
     inputfile.seek(int(sys.argv[2]))
     print(inputfile.read())
 except:
-    print("Errors occurred for location...")
+    print("Errors accured for location...")
